179161cffc4fd92d76bd387fe5f87697a4d8f853
Yosef-ft <yosefetene02@gmail.com>
2024-08-03 13:45:31 +0300

    Siinqee bank data to database

--- RateScraper/RateScraper/spiders/Siinqee.py
+++ RateScraper/RateScraper/spiders/Siinqee.py
@@ -1,5 +1,7 @@
-import scrapy
+import datetime
 
+import scrapy
+from RateScraper.items import ExchangeItem
 
 class SiinqeeSpider(scrapy.Spider):
     name = "Siinqee"
@@ -7,4 +9,27 @@ class SiinqeeSpider(scrapy.Spider):
     start_urls = ["https://siinqeebank.com/#/"]
 
     def parse(self, response):
-        pass
+        
+        data = response.css('table tr td::text').getall()
+
+        Rates = []
+        temp = []
+        for rate in data:
+            temp.append(rate)
+            if len(temp) == 3:
+                Rates.append(temp)
+                temp = []  
+
+        ## The website doesn't provide when the data has been updated
+        now = datetime.datetime.now()
+        date_string = now.strftime('%B %d, %Y')                
+
+        for rate in Rates:
+            exchange_rate = ExchangeItem()
+            exchange_rate['bank'] = self.name
+            exchange_rate['Date'] = date_string
+            exchange_rate['CurrencyCode'] = rate[0]
+            exchange_rate['Buying'] = rate[1]
+            exchange_rate['Selling'] = rate[2]
+
+            yield exchange_rate
