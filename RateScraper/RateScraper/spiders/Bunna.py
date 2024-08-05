8b3b9b8e5b846b18628d98f547091f2607bcf686
Yosef-ft <yosefetene02@gmail.com>
2024-08-05 12:49:52 +0300

    Scraping Bunna bank updated to their recent websire)

--- RateScraper/RateScraper/spiders/Bunna.py
+++ RateScraper/RateScraper/spiders/Bunna.py
@@ -1,36 +1,37 @@
 import datetime
 
 import scrapy
-from RateScraper.items import ExchangeItem
+from RateScraper.items import FullExchangeItems
 
 class BunnaSpider(scrapy.Spider):
     name = "Bunna"
     allowed_domains = ["bunnabanksc.com"]
-    start_urls = ["https://bunnabanksc.com/foreign-exchange/"]
-
+    start_urls = ["https://bunnabanksc.com/"]
+  
     def parse(self, response):
         
-        data = response.css('table tbody tr td::text').getall()
+        data = response.css('table tbody tr td div span::text').getall()
+        data = [rate.replace('\t', '').replace('\n','') for rate in data]
+        data = [rate for rate in data if rate != ' ' and rate != '' ]
 
         Rates = []
         temp = []
         for rate in data:
             temp.append(rate)
-            if len(temp) == 3:
+            if len(temp) == 5:
                 Rates.append(temp)
                 temp = []             
-
-        ## The website doesn't provide when the data has been updated
-        now = datetime.datetime.now()
-        date_string = now.strftime('%B %d, %Y')                          
+                    
 
         ## Constantly changing their website this is just half the data
-        for rate in Rates[:3]:
-            exchange_rate = ExchangeItem()            
+        for rate in Rates:
+            exchange_rate = FullExchangeItems()            
             exchange_rate['bank'] = self.name
-            exchange_rate['Date'] = date_string
-            exchange_rate["CurrencyCode"] = rate[0]
-            exchange_rate["Buying"] = rate[1]
-            exchange_rate["Selling"] = rate[2]     
+            exchange_rate['Date'] = response.css('div.elementor-element div.elementor-widget-container h4::text').get()
+            exchange_rate['CurrencyCode'] = rate[0]
+            exchange_rate['CashBuying']= rate[1]
+            exchange_rate['CashSelling'] = rate[2]
+            exchange_rate['TransactionalBuying'] = rate[3]
+            exchange_rate['TransactionalSelling'] = rate[4]  
 
             yield exchange_rate          
